a521a2e940cd4481c77d53299051d8b1a7090536
dhanushka365 <technohub10000@gmail.com>
2023-04-14 15:36:06 +0530

    user controller implemented

---
+++ src/main/java/com/example/ServerUserManagemnt/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.example.ServerUserManagemnt.controller;
+
+import com.example.ServerUserManagemnt.model.Role;
+import com.example.ServerUserManagemnt.model.User;
+import com.example.ServerUserManagemnt.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/api/user/registration")
+    public ResponseEntity<?> register(@RequestBody User user){
+        if (userService.findByUsername(user.getUsername()) != null){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        user.setRole(Role.USER);
+        return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/api/user/login")
+    public ResponseEntity<?> login(Principal principal){
+        if(principal == null){
+            return ResponseEntity.ok(principal);
+        }
+        return new ResponseEntity<>(userService.findByUsername(principal.getName()),HttpStatus.OK);
+    }
+
+}
