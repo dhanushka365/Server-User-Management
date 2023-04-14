0ebae2a0ff40ed68100607cb1279820cb383a75f
dhanushka365 <technohub10000@gmail.com>
2023-04-14 11:36:31 +0530

    Initial commit

---
+++ src/main/java/com/example/ServerUserManagemnt/ServerUserManagemntApplication.java
@@ -0,0 +1,13 @@
+package com.example.ServerUserManagemnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerUserManagemntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerUserManagemntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ServerUserManagemnt/ServerUserManagemntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ServerUserManagemnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerUserManagemntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
