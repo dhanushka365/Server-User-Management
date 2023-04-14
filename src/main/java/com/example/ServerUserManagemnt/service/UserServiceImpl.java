f163deb5bcaef942940bfcbaa780d6affc12fd72
dhanushka365 <technohub10000@gmail.com>
2023-04-14 17:59:29 +0530

    errors resolved

--- src/main/java/com/example/ServerUserManagemnt/service/UserServiceImpl.java
+++ src/main/java/com/example/ServerUserManagemnt/service/UserServiceImpl.java
@@ -16,7 +16,7 @@ public class UserServiceImpl implements UserService{
     @Autowired
     private UserRepository userRepository;
 
-
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Override
