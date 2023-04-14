aa47e2d609f6ab781ae3c383b5ca6eaec99cbecc
dhanushka365 <technohub10000@gmail.com>
2023-04-14 15:40:49 +0530

    Admin controller implemented

---
+++ src/main/java/com/example/ServerUserManagemnt/controller/AdminController.java
@@ -0,0 +1,18 @@
+package com.example.ServerUserManagemnt.controller;
+
+import com.example.ServerUserManagemnt.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/api/admin/all")
+    public ResponseEntity<?> findAllUsers(){
+        return ResponseEntity.ok(userService.findAllUsers());
+    }
+}
